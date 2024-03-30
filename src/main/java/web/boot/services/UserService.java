71fb17184ac16280e930f624c48fa0bc4684cb93
Somibo <emiltmil@gmail.com>
2024-03-30 16:10:27 +0300

    Initial commit

---
+++ src/main/java/web/boot/services/UserService.java
@@ -0,0 +1,48 @@
+package web.boot.services;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.boot.models.User;
+import web.boot.repositories.UserRepository;
+;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+@Transactional(readOnly = true)
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public Object findOne(int id) {
+        Optional<User> foundUser = userRepository.findById(id);
+        return foundUser.orElse(null);
+    }
+
+    @Transactional
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public void update(int id, @Valid User updatedUser) {
+        updatedUser.setId(id);
+        userRepository.save(updatedUser);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        userRepository.deleteById(id);
+    }
+}
